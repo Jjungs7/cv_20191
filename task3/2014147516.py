c86aec339f0c93fe81157cc5a82ffbfdf436c6b7
jjungs <jeongy95@naver.com>
2019-05-01 21:14:01 +0900

    자료형 수정

--- task3/2014147516.py
+++ task3/2014147516.py
@@ -45,9 +45,8 @@ def main(args):
     for idx, image in enumerate(images_train):
         for i in range(image.shape[0]):
             images_train_2d[image.shape[1] * i: image.shape[1] * (i + 1), idx] = np.array(image[i, :], dtype=np.int)
-    images_train_2d = np.array(images_train_2d)
 
-    means = np.array([])
+    means = np.array([], dtype=np.float)
     for i in range(images_train_2d.shape[0]):
         mean = 0
         for j in range(39):
@@ -58,6 +57,7 @@ def main(args):
         means = np.append(means, mean)
 
     U, s, Vt, d = svd(images_train_2d, percentage)
+    eigen_face = U[:, :d]
 
     f.write(f'Input Percentage: {percentage}\n')
     f.write(f'Selected Dimension: {d}\n')
@@ -66,10 +66,19 @@ def main(args):
         for j in range(39):
             result[i, j] += means[i]
 
+    '''
+    result = np.zeros((image_h * image_w, 39))
+    for idx, image in enumerate(images_train):
+        image = np.reshape(image, image.shape[0] * image.shape[1]) - means
+        res = np.zeros(image_h * image_w)
+        for i in range(d):
+            res += np.dot(eigen_face[:, i], np.dot(eigen_face[:, i].T, image))
+        result[:, idx] = means + res
+    '''
+
     for idx, file_name in enumerate(train_file_names):
         cv2.imwrite(f'./{STUDENT_CODE}/{file_name}', np.reshape(result[:, idx], (image_h, image_w)))
 
-    eigen_face = U[:, :d]
 
     """
     ###### STEP 2 #######
@@ -77,7 +86,7 @@ def main(args):
     """
     f.write('\n##########  STEP 2  ##########\n')
     results = []
-    computed_images = np.array([cv2.imread(f'./{STUDENT_CODE}/{t}', cv2.IMREAD_GRAYSCALE) for t in train_file_names])
+    computed_images = np.array([cv2.imread(f'./{STUDENT_CODE}/{t}', cv2.IMREAD_GRAYSCALE) for t in train_file_names], dtype=np.int)
     for i in range(len(computed_images)):
         difference = np.mean((images_train[i] - computed_images[i]) ** 2)
         results.append(difference)
@@ -92,7 +101,7 @@ def main(args):
     Face recognition using l2 distance
     """
     f.write('\n##########  STEP 3  ##########\n')
-    images_test = np.array([cv2.imread(f'./faces_test/{t}', cv2.IMREAD_GRAYSCALE) for t in test_file_name])
+    images_test = np.array([cv2.imread(f'./faces_test/{t}', cv2.IMREAD_GRAYSCALE) for t in test_file_name], dtype=np.int)
     for idx, image in enumerate(images_test):
         image = np.reshape(image, image.shape[0] * image.shape[1]) - means
         res = np.zeros(image_h * image_w)
